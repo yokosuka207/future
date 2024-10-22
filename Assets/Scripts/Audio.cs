33b1f8e8e1e5a56ab8d2c9c189c1ab97ffa001e8
yokosuka <yokosuka.207@icloud.com>
2024-10-22 23:32:23 +0900

    SampleAudio

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+
+    float speed = 3.0f;
+
+    void Update()
+    {
+        // Wキー（前方移動）
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += speed * transform.forward * Time.deltaTime;
+        }
+
+        // Sキー（後方移動）
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= speed * transform.forward * Time.deltaTime;
+        }
+
+        // Dキー（右移動）
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += speed * transform.right * Time.deltaTime;
+        }
+
+        // Aキー（左移動）
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= speed * transform.right * Time.deltaTime;
+        }
+    }
+}
